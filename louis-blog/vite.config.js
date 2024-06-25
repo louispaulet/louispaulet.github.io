58a3bb7e1b5dcf3aa2d55f7242e72f3aa9de4a3a
louispaulet <louispaulet13@gmail.com>
2024-06-25 18:17:49 +0200

    create CV corner to see and download CVs and add buttons in homepage to access it, rework homepage to prevent style changes while the articles load
    side by side cv view, set URL as param for PDF viewer component
    
    move CV display to the EN version above and the FR version below, add temp titles, add page turn buttons and download button
    
    make PDFs bigger
    
    add smooth scrolling to the correct CV section
    
    center buttons prev and next on pdf viewer
    
    replace default pdf open with a download with the filename of the pdf when the download button is clicked
    
    add a button on homepage to go to the CV corner
    
    update home to make sure the articles dont blink when coming back from CV corner
    
    add social links and funny quote to the CV corner

--- louis-blog/vite.config.js
+++ louis-blog/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // replace with your base URL when deploying
+  base: './', // replace with your base URL when deploying
   plugins: [react()],
   resolve: {
     alias: {
