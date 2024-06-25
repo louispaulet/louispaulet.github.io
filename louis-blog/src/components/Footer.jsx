433b6b6d3fcfce30a8a89eff5cb55676f18ba57c
louispaulet <louispaulet13@gmail.com>
2024-06-25 12:34:37 +0200

    finally updated the footer to include the website name

--- louis-blog/src/components/Footer.jsx
+++ louis-blog/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-blue-600 p-4 text-white mt-auto">
       <div className="container mx-auto text-center">
-        &copy; {new Date().getFullYear()} Blog. All rights reserved.
+        &copy; {new Date().getFullYear()} Louis Paulet Blog. All rights reserved.
       </div>
     </footer>
   );
