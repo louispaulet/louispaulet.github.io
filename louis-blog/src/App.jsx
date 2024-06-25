6dbaf9e784a31920ef2cc37c832c4e3f48ad7b88
louispaulet <louispaulet13@gmail.com>
2024-06-25 21:49:22 +0200

    move pdf heavy component to lazy loading for improved build time

--- louis-blog/src/App.jsx
+++ louis-blog/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
-import CvViewer from './pages/CvViewer';
 import Post from './components/Post';
 
+// Dynamic import for CvViewer (heavy!)
+const CvViewer = lazy(() => import('./pages/CvViewer'));
 
 const App = () => {
   return (
@@ -15,13 +16,15 @@ const App = () => {
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow container mx-auto p-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cv" element={<CvViewer />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/posts/:postId" element={<Post />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cv" element={<CvViewer />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/posts/:postId" element={<Post />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
       </div>
