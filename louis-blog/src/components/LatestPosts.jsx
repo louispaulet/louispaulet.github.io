0af390123107c2e26ad3da006f248673c78b57e7
louispaulet <louispaulet13@gmail.com>
2024-06-26 14:57:24 +0200

    refactor latest posts display in home by moving it to a dedicated component and loading JSON data from import statement to enjoy a cache speedup
    converted website icon to webp and moved the navbar website icon to the cached_images repo (discovered issue while working on posts delay)
    
    move json list of posts to a cached path
    
    add optional delay when loading in case we want to add a loader instead of placeholders later

---
+++ louis-blog/src/components/LatestPosts.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import PostPreview from './PostPreview';
+import { FaNewspaper } from "react-icons/fa";
+import { FcReading } from "react-icons/fc";
+import postsData from './../posts.json';
+
+const LatestPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setPosts(postsData);
+    // optional delay -- still not decided if placeholders are better than a blink...
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }, []);
+
+  return (
+    <div className="w-full">
+      <h2 className="text-2xl lg:text-3xl font-semibold mb-6 bg-black text-white inline-block py-1 px-1">Recent Posts</h2>
+      <ul>
+        {loading ? (
+          Array.from({ length: 6 }).map((_, index) => (
+            <li key={index} className="mb-4">
+              <div className="block p-6 bg-gray-100 rounded-lg shadow animate-pulse w-full">
+                <div className="flex">
+                  <div className="flex-shrink-0 mr-4">
+                    <FaNewspaper className="text-6xl text-gray-300" />
+                  </div>
+                  <div className="flex-grow">
+                    <div className="h-6 bg-gray-300 rounded w-3/4 mb-2"></div>
+                    <div className="h-4 bg-gray-300 rounded w-1/2 mb-2"></div>
+                    <div className="h-4 bg-gray-300 rounded w-1/3 mb-2"></div>
+                    <div className="h-5 bg-gray-300 rounded w-1/4 mt-2 flex items-center">
+                      <span className="mr-1">Read more</span>
+                      <FcReading className="text-2xl" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))
+        ) : (
+          posts.map((post) => (
+            <PostPreview
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              summary={post.summary}
+              postDate={post.postDate}
+            />
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default LatestPosts;
--- louis-blog/src/components/Navbar.jsx
+++ louis-blog/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import website_logo from './../cached_images/logo_favicon.webp'
 
 const Navbar = () => {
   return (
@@ -7,7 +8,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Link to="/">
-            <img src="logo_favicon.png" alt="Louis Paulet" className="w-10 h-10 border-2 border-white" />
+            <img src={website_logo} alt="Louis Paulet" className="w-10 h-10 border-2 border-white" />
           </Link>
           <Link to="/" className="text-2xl font-bold">Louis Paulet Blog</Link>
         </div>
