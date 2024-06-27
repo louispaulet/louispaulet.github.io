36c9acb7f1ef9155cce534c7da1f1fb154d213c6
louispaulet <louispaulet13@gmail.com>
2024-06-28 00:23:35 +0200

    remove black border from navbar and title logo, try alternative design but reverted, all PSD files included

--- louis-blog/src/components/Navbar.jsx
+++ louis-blog/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import website_logo from './../cached_images/logo_favicon.webp'
+import website_logo from './../cached_images/logo_favicon_white_blue.webp'
 
 const Navbar = () => {
   return (
@@ -8,7 +8,9 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Link to="/">
-            <img src={website_logo} alt="Louis Paulet" className="w-10 h-10 border-2 border-white" />
+          <div >
+            <img src={website_logo} alt="Louis Paulet" className="w-10 h-10 border-0 border-white" />
+           </div>
           </Link>
           <Link to="/" className="text-2xl font-bold">Louis Paulet Blog</Link>
         </div>
