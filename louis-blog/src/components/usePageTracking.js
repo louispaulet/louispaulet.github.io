744a82e934d6cba8d784f38195b4c1c36934d100
louispaulet <louispaulet13@gmail.com>
2024-07-04 12:10:11 +0200

    update pageview tags to get a path and a title

--- louis-blog/src/components/usePageTracking.js
+++ louis-blog/src/components/usePageTracking.js
@@ -5,8 +5,17 @@ export const UsePageTracking = () => {
   const location = useLocation();
 
   useEffect(() => {
+    const fullPath = location.pathname + location.search + location.hash;
+    let lastWord = fullPath.split("/").filter(Boolean).pop();
+
+    // Set lastWord to "homepage" if it is empty or undefined
+    if (!lastWord) {
+      lastWord = "homepage";
+    }
+
     window.gtag("event", "page_view", {
-      page_path: location.pathname + location.search + location.hash
+      page_title: lastWord,
+      page_location: fullPath
     });
   }, [location]);
 };
