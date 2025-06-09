d4ab8ea36aa0ab56c8ac97979b18e6698eb2f2c6
the_french_artist <louispaulet@users.noreply.github.com>
2025-06-09 23:10:56 +0200

    Use React Router Link in NotFound

--- louis-blog/src/pages/NotFound.jsx
+++ louis-blog/src/pages/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="container mx-auto p-4 text-center">
       <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
       <p className="text-lg text-gray-700">Sorry, the page you are looking for does not exist.</p>
-      <a href="#/" className="text-blue-500 mt-4 inline-block">Go back to Home</a>
+      <Link to="/" className="text-blue-500 mt-4 inline-block">Go back to Home</Link>
     </div>
   );
 };
